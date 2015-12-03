937d05488d03d9518aeb23c31ea45ebd405cd017
Petlefeu <nicolas_beguier@hotmail.com>
2015-12-03 08:35:49 +0100

    Version 1.1.0

--- api_lbc.py
+++ api_lbc.py
@@ -6,13 +6,20 @@ from pdb import set_trace as st
 import subprocess
 import bs4 as BeautifulSoup
 import re
+import time
+import datetime
 
 username = sys.argv[1]
 password = sys.argv[2]
+force_header = sys.argv[3]
 cookie_jar = './cookie_api_lbc.jar'
 
-popen = subprocess.Popen(('./page_gen.sh', username, password), stdout=subprocess.PIPE)
+ts = time.time()
+timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d%H%M%S')
 
+page_filename = './page.html'
+
+popen = subprocess.Popen(('./page_gen.sh', username, password, page_filename), stdout=subprocess.PIPE)
 popen.wait()
 
 f = open('./page.html', 'r')
@@ -22,16 +29,51 @@ f.close()
 objects = {}
 for i, title in enumerate(soup.find_all('div', 'title')):
     o = {}
-    o['title'] = title.a.contents[0]
+    o['title'] = str(title.a.contents[0].encode('utf-8').decode('ascii', 'ignore'))
     objects[i] = o
 
 for i, price in enumerate(soup.find_all('div', 'price')):
     objects[i]['price'] = int(price.contents[0].encode('utf-8').replace(' \xe2\x82\xac', ''))
 
 for i in range(len(objects)):
-    objects[i]['vues'] = int(soup("span", "square")[i*3].contents[0])
-    objects[i]['mails'] = int(soup("span", "square")[i*3+1].contents[0])
-    objects[i]['clics'] = int(soup("span", "square")[i*3+2].contents[0])
+    objects[i]['vues'] = int(soup('span', 'square')[i*3].contents[0])
+    objects[i]['mails'] = int(soup('span', 'square')[i*3+1].contents[0])
+    objects[i]['clics'] = int(soup('span', 'square')[i*3+2].contents[0])
 
-st()
-# grep -a 'class="title"' page.html  | awk -F '12_s">' '{print $2}' | awk -F '</a>' '{print $1}'
+
+# VUES
+if force_header == 'True':
+    print 'nom;prix;%s' % timestamp
+else:
+    print timestamp
+for i in range(len(objects)):
+    if force_header == 'True':
+        print '%s;%s;%s' % (objects[i]['title'], objects[i]['price'], objects[i]['vues'])
+    else:
+        print '%s' % objects[i]['vues']
+
+print ''
+
+# CLICS
+if force_header == 'True':
+    print 'nom;prix;%s' % timestamp
+else:
+    print timestamp
+for i in range(len(objects)):
+    if force_header == 'True':
+        print '%s;%s;%s' % (objects[i]['title'], objects[i]['price'], objects[i]['clics'])
+    else:
+        print '%s' % objects[i]['clics']
+
+print ''
+
+# MAILS
+if force_header == 'True':
+    print 'nom;prix;%s' % timestamp
+else:
+    print timestamp
+for i in range(len(objects)):
+    if force_header == 'True':
+        print '%s;%s;%s' % (objects[i]['title'], objects[i]['price'], objects[i]['mails'])
+    else:
+        print '%s' % objects[i]['mails']
--- cookie_gen.sh
+++ cookie_gen.sh
@@ -13,7 +13,6 @@ curl -s -b ${COOKIE_JAR} -c ${COOKIE_JAR} -X POST --data "st_username=${USERNAME
 cookie_gen
 
 # Première requête
-HEADERS_2=$(echo -H 'Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'  -H 'Accept-Language:fr-FR,fr;q=0.8,en-US;q=0.6,en;q=0.' -H 'Cookie:cookieFrame=2;s=red1xb6d79b789f7b65c88305075b9e8ff3972f32ee98;xtvrn=$266818$' -H 'DNT:1' -H 'Host:compteperso.leboncoin.fr' -H 'Referer:http://www.leboncoin.fr/annonces/offres/ile_de_france/' -H 'Upgrade-Insecure-Requests:1' -H 'User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36')
-curl -k -s -L ${HEADERS_2} https://compteperso.leboncoin.fr/account/index.html?ca=12_s >/dev/null 2>&1
+curl -k -s -L -b ${COOKIE_JAR} https://compteperso.leboncoin.fr/account/index.html?ca=12_s >/dev/null 2>&1
 
 cookie_gen
